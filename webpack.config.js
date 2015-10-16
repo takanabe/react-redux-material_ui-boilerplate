e3243e55e6e4bc80167499ff975df2203294e317
Takayuki WATANABE <takanabe.w@gmail.com>
2015-10-16 15:31:40 +0900

    Add ESLint, Git and Webpack configurations.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  context: __dirname + "/src",
+  entry: {
+    jsx: "./index.jsx",
+    css: "./main.css",
+    html: "./index.html",
+  },
+
+  output: {
+    path: __dirname + "/static",
+    filename: "bundle.js",
+  },
+  module: {
+    preLoaders: [
+        //Eslint loader
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader"},
+    ],
+    loaders: [
+      { test: /\.html$/, loader: "file?name=[name].[ext]" },
+      { test: /\.css$/, loader: "file?name=[name].[ext]" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader?stage=0&optional=runtime"]},
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  eslint: {
+    configFile: './.eslintrc'
+  },
+};
